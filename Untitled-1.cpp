db8e8243f4f3657c2411552b00e6077b35e94d6b
Your NameProgrammingm <niamulhossen223@gmail.com>
2025-01-11 18:47:21 +0600

    first commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x,y;
+cout<<"Enter the anny number";
+cin>>x>>y;
+int sum = x+y;
+cout<<"this sum is number "<<sum;
+
+
+    return 0;
+}
